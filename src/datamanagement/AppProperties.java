abb183b095841f72cc7db7561d74525699e0bffc
Praveen <sankalpa1994@gmail.com>
2016-09-04 21:23:14 -0700

    changed AppProperties.java file

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -2,33 +2,33 @@ package datamanagement;
 import java.util.*;
 import java.io.*;
 
-public class AppProperties 
-{
-	private static AppProperties self = null;
-	private Properties properties;
+		public class AppProperties 
+		{
+			private static AppProperties self = null;//static object for app proprties
+			private Properties properties;
 
-	public static AppProperties getInstance() 
-	{
-		if (self == null ) 
+		public static AppProperties getInstance() 
+		{
+			if (self == null ) 
 		{ 
-			self = new AppProperties(); 
+			self = new AppProperties(); //initiating AppProperties object in case its not null
 		} 
-		return self;
-	}
-	private AppProperties() 
-	{
-		properties = new Properties();
-		try 
-		{
-			properties.load(new FileInputStream("Properties.prop"));
-		} 
-		catch (IOException e) 
-		{
-			throw new RuntimeException("Could not read property file");
+			return self;
 		}
-	}
-    public Properties getProperties() 
-    {
-    	return properties;
-    }
+		private AppProperties() 
+		{
+			properties = new Properties();
+			try 
+			{
+				properties.load(new FileInputStream("Properties.prop"));//loading properties from file properties.prop 
+			} 
+			catch (IOException e) 
+			{
+				throw new RuntimeException("Could not read property file");// throw exception in case file not found
+			}
+		}
+		public Properties getProperties()
+		{
+			return properties;//return properties getting from file
+		}
 }
